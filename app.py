66015f72735761bd784552dccc84ae6cd5a293e2
Ferry Ezra <164129942+Ferry-Ezra@users.noreply.github.com>
2026-05-29 01:56:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import pymysql
+pymysql.install_as_MySQLdb()
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify, Response
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
